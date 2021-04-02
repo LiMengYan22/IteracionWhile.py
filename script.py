9e76bdc1485d64fe00dc815b68c9530a3135222f
LiMengYan <80227002+yanlimeng@users.noreply.github.com>
2021-04-02 09:31:33 +0200

    Create script.py

---
+++ script.py
@@ -0,0 +1,6 @@
+# Cuenta atras de 5 a 0 y fuego..
+n = 5
+while n > 0: # mientras n sea mayor que cero
+print n # muestra el valor de n
+n = n-1 # lo reduce una unidad y al llegar a cero
+print 'Fuego!!' # imprime..
